5ddfaf770f60128839cc3121ee50b6065509529e
ed5v <126750252+ed5v@users.noreply.github.com>
2025-06-05 21:49:29 -0600

    Update urls.py

--- SIRENITA/urls.py
+++ SIRENITA/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from SIRENITA import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    #path('', views.home, name="home"),
     
+    path('', views.ORDEN, name='tabla'),
+    path('accion/<int:numero>/',views.agregar_orden, name='agregar'),
 ]
